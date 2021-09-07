03f5b6505298a20175a57732d5a76977fe192312
Sergey Kluchkovsky <kaineer@htmlacademy.ru>
2021-09-07 16:15:11 +0500

    Перекрывает содержимое редактора виджетом
    чтобы отлавливать события от мыши
    
    Кроме того не даёт фокусу уйти из редактора.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ var opts = {
 
 var editor = monaco.editor.create(el, opts);
 
+let selection = null;
+
 /**
  * Run fn after interval ms
  *
@@ -40,26 +42,95 @@ const typeChars = async (value, interval = 100) => {
 
 const typeOneChar = (char) => {
   const edit = {
-    range: editor.getSelection(),
+    range: selection || editor.getSelection(),
     text: char,
     forceMoveMarkers: true
   };
-  editor.executeEdits(null, [edit]);
+
+  editor.getModel().pushEditOperations(
+    editor.getSelections(),
+    [edit],
+    () => null
+  );
+
+  selection = editor.getSelection();
+};
+
+const createBlocker = (editor) => {
+  const widget = {
+    domNode: null,
+    getId() {
+      return 'dom.blocker';
+    },
+    getDomNode() {
+      if (!this.domNode) {
+        this.domNode = document.createElement('div');
+        const style = this.domNode.style;
+        const br = editor.getDomNode().getBoundingClientRect();
+
+        Object.assign(style, {
+          top: '0px',
+          left: '0px',
+          width: br.width + 'px',
+          height: br.height + 'px',
+        });
+
+        ['mousedown', 'mouseup', 'click', 'mousemove'].forEach((name) => {
+          this.domNode.addEventListener(name, (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+          }, true);
+        });
+      }
+
+      return this.domNode;
+    },
+    getPosition() {
+      return {
+        position: {
+          lineNumber: 0,
+          column: 0
+        },
+        preference: [
+          monaco.editor.ContentWidgetPositionPreference.ABOVE,
+          monaco.editor.ContentWidgetPositionPreference.BELOW
+        ]
+      }
+    }
+  };
+
+  return widget;
 };
 
 editor.focus();
 (async () => {
+  editor.updateOptions({readOnly: true});
+
+  const blocker = createBlocker(editor);
+  editor.addOverlayWidget(blocker);
+
+  const unsubscribe = editor.onDidBlurEditorText(() => {
+    editor.focus();
+  });
+
   await typeChars('// * Adding some code\n\n', 50);
   await typeChars('const answer = 42;');
 
   editor.setPosition({lineNumber: 2, column: 1});
+  selection = editor.getSelection();
+
   await typeChars('// * Removing some code', 50);
+
   editor.setSelection({
     startColumn: 1,
     endColumn: 19,
     startLineNumber: 3,
     endLineNumber: 3
   });
+  selection = {
+    lineNumber: 3,
+    column: 1
+  };
 
   typeOneChar('');
 
@@ -70,5 +141,15 @@ editor.focus();
     startLineNumber: 5,
     endLineNumber: 5
   });
+  selection = {
+    lineNumber: 5,
+    column: 1
+  };
+
+  unsubscribe.dispose();
+
+  editor.removeOverlayWidget(blocker);
+
+  editor.updateOptions({readOnly: false});
 })();
 
